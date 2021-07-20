b9a71977a731596bbb3499f956872838638a99ea
Kevin Zeillmann <kzeillmann@gmail.com>
2021-07-20 19:30:17 -0400

    First pass at refactoring

--- gilded-rose.js
+++ gilded-rose.js
@@ -15,55 +15,71 @@
  * ]);
  */
 
+const calculateSellIn = (item) => {
+  if (item.name == "Sulfuras, Hand of Ragnaros") {
+    return item.sellIn;
+  }
+  return item.sellIn - 1;
+}
+
+const decrementQuality = (item) => {
+  const defaultSubtract = 1;
+  let multiplier = 1;
+  if(item.name === "Conjured Mana Cake") {
+    multiplier = multiplier * 2;
+  }
+  if(item.sellIn <= 0) {
+    multiplier = multiplier * 2
+  }
+  const calculatedQuality = item.quality - (defaultSubtract * multiplier);
+
+  return Math.max(0, calculatedQuality);
+  
+};
+
 module.exports = function updateQuality(items) {
-  for (var i = 0; i < items.length; i++) {
+  return items.map((item) => {
+    if(item.name === "Sulfuras, Hand of Ragnaros") {
+      return item;
+    }
+
+    const newSellIn = calculateSellIn(item);
+
     if (
-      items[i].name != "Aged Brie" &&
-      items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+      item.name != "Aged Brie" &&
+      item.name != "Backstage passes to a TAFKAL80ETC concert"
     ) {
-      if (items[i].quality > 0) {
-        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-          items[i].quality = items[i].quality - 1;
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1;
-        if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (items[i].sellIn < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
+        item.quality = decrementQuality(item);
+    } else { //items that increase in value
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
           }
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
+          if (item.sellIn < 6 && item.quality < 50) {
+              item.quality = item.quality + 1;
           }
         }
       }
     }
-    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-      items[i].sellIn = items[i].sellIn - 1;
-    }
-    if (items[i].sellIn < 0) {
-      if (items[i].name != "Aged Brie") {
-        if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (items[i].quality > 0) {
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-              items[i].quality = items[i].quality - 1;
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality;
+    item.sellIn = newSellIn;
+
+    if (item.sellIn < 0) {
+      if (item.name == "Aged Brie") {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
         }
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
+        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+          item.quality = 0;
         }
       }
     }
-  }
 
-  return items;
+    return item;
+  });
+
 };
--- gilded-rose.test.js
+++ gilded-rose.test.js
@@ -3,3 +3,63 @@ const updateQuality = require("./");
 test("should do something", () => {
   updateQuality([]);
 });
+
+// todo: make test more clear
+test("should update quality non-conjured items", () => {
+  const result = updateQuality([
+       { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
+       { name: "Aged Brie", sellIn: 2, quality: 0 },
+       { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+       { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
+       {
+        name: "Backstage passes to a TAFKAL80ETC concert",
+         sellIn: 15,
+         quality: 20
+       }
+     ]);
+
+     expect(result).toEqual([
+       {"name": "+5 Dexterity Vest", "quality": 19, "sellIn": 9}, 
+       {"name": "Aged Brie", "quality": 1, "sellIn": 1},
+       {"name": "Elixir of the Mongoose", "quality": 6, "sellIn": 4}, 
+       {"name": "Sulfuras, Hand of Ragnaros", "quality": 80, "sellIn": 0}, 
+       {"name": "Backstage passes to a TAFKAL80ETC concert", "quality": 21, "sellIn": 14}
+      ]);
+});
+
+test("should update quality with conjured items", () => {
+  const result = updateQuality([
+    { name: "Conjured Mana Cake", sellIn: 3, quality: 6 }
+  ]);
+
+  expect(result).toEqual( [{"name": "Conjured Mana Cake", "quality": 4, "sellIn": 2}])
+});
+
+test("should update quality of conjured items with 0 sellIn", () => {
+  const result = updateQuality([
+    { name: "Conjured Mana Cake", sellIn: 0, quality: 1 }
+  ]);
+  expect(result).toEqual([{ name: "Conjured Mana Cake", sellIn: -1, quality: 0 }])
+});
+
+
+test("should update quality of conjured items with <0 sellIn", () => {
+  const result = updateQuality([
+    { name: "Conjured Mana Cake", sellIn: -1, quality: 1 }
+  ]);
+  expect(result).toEqual([{ name: "Conjured Mana Cake", sellIn: -2, quality: 0 }])
+});
+
+test("should update quality with item with < 0 days sellIn value", () => {
+
+});
+
+test("should not update Sulfuras, Hand of Ragnaros", () => {
+  const result = updateQuality([
+    { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
+  ]);
+
+  expect(result).toEqual([{"name": "Sulfuras, Hand of Ragnaros", "quality": 80, "sellIn": 0}]);
+});
+
+//ensure negatives are handled
